6d8f11253a424ac6f186ec82b2c8b655c6aa5737
C4 <29991504+TheIllusiveC4@users.noreply.github.com>
2022-03-29 19:18:21 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Curious Lights
+[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg?&style=flat-square)](https://www.gnu.org/licenses/lgpl-3.0)
+[![ko-fi](https://img.shields.io/badge/Support%20Me-Ko--fi-%23FF5E5B?style=flat-square)](https://ko-fi.com/C0C1NL4O)
+
+Curious Lights is a mod that acts as a simple compatibility bridge between [Curios API](https://www.curseforge.com/minecraft/mc-mods/curios)
+and dynamic light mods. With this mod, items in curios slots will inherit the dynamic lighting behavior defined by the
+dynamic lighting mod. As an example, if a held torch would provide dynamic lighting, then installing this mod will cause
+torches placed in curios slots to also provide dynamic lighting.
+
+**Compatible Mods**
+- [Dynamic Lights](https://www.curseforge.com/minecraft/mc-mods/dynamic-lights)
+- [Lucent](https://www.curseforge.com/minecraft/mc-mods/lucent)
+
+Note: This mod's sidedness depends on the dynamic lighting mod used. For example, if using Lucent then this mod is
+client-side only because Lucent is client-side only. On the other hand, if using Dynamic Lights then this mod is
+required on both client and server because Dynamic Lights is required on both as well.
+
+## Downloads
+**CurseForge**
+- [![](http://cf.way2muchnoise.eu/short_curious-lights_downloads%20on%20Forge.svg)](https://www.curseforge.com/minecraft/mc-mods/curious-lights/files) [![](http://cf.way2muchnoise.eu/versions/curious-lights.svg)](https://www.curseforge.com/minecraft/mc-mods/curious-lights)
+
+## Support
+
+Please report all bugs, issues, and feature requests to the [issue tracker](https://github.com/TheIllusiveC4/CuriousLights/issues).
+
+For non-technical support and questions, join the developer's [Discord](https://discord.gg/JWgrdwt).
+
+## License
+
+All source code and assets are licensed under LGPL 3.0.
+
+## Donations
+
+Donations to the developer can be sent through [Ko-fi](https://ko-fi.com/C0C1NL4O).
+
+## Affiliates
+
+[![BisectHosting](https://i.ibb.co/1G4QPdc/bh-illusive.png)](https://bisecthosting.com/illusive)
---
+++ src/main/java/top/theillusivec4/curiouslights/CuriousLights.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2022 C4
+ *
+ * This file is part of Curious Lights.
+ *
+ * Curious Lights is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Curious Lights is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * and the GNU Lesser General Public License along with Curious Lights.
+ * If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+package top.theillusivec4.curiouslights;
+
+import java.util.Objects;
+import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.network.NetworkConstants;
+import top.theillusivec4.curiouslights.dynamiclights.DynamicLightsModule;
+
+@Mod(CuriousLights.MOD_ID)
+public class CuriousLights {
+
+  public static final String MOD_ID = "curiouslights";
+
+  private static boolean isDynamicLightsLoaded = false;
+
+  public CuriousLights() {
+    isDynamicLightsLoaded = ModList.get().isLoaded("dynamiclights");
+    FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+    ModLoadingContext context = ModLoadingContext.get();
+    context.registerExtensionPoint(IExtensionPoint.DisplayTest.class,
+        () -> new IExtensionPoint.DisplayTest(() -> getRemoteVersion(context),
+            (incoming, isNetwork) -> acceptsServer(context, incoming)));
+  }
+
+  private void setup(final FMLCommonSetupEvent event) {
+
+    if (isDynamicLightsLoaded) {
+      DynamicLightsModule.setup();
+    }
+  }
+
+  private String getRemoteVersion(ModLoadingContext context) {
+
+    if (isDynamicLightsLoaded) {
+      return context.getActiveContainer().getModInfo().getVersion().toString();
+    }
+    return NetworkConstants.IGNORESERVERONLY;
+  }
+
+  private boolean acceptsServer(ModLoadingContext context, String incoming) {
+
+    if (isDynamicLightsLoaded) {
+      return Objects.equals(incoming,
+          context.getActiveContainer().getModInfo().getVersion().toString());
+    }
+    return true;
+  }
+}
